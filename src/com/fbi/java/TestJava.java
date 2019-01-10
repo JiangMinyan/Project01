129b66a720781ffa0667be4183b84b5fd7251fc2
max <max.jiang@fbi-china.com>
2019-01-10 11:59:44 +0800

    add TestJava File

---
+++ src/com/fbi/java/TestJava.java
@@ -0,0 +1,11 @@
+package com.fbi.java;
+
+/**
+ * @author max
+ * @create 2019-01-10 11:58
+ */
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("TestJava File");
+    }
+}
